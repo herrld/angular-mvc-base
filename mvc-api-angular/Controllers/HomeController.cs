6bbf643424b9e57c2d84275d6b1cb8cef665caf5
herrld <lee.herring@enspiresoftware.com>
2018-07-07 20:23:22 -0500

    initial

---
+++ mvc-api-angular/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_api_angular.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return File(Server.MapPath("/dist/") + "index.html", "text/html");
+            //return View();
+        }
+    }
+}
---
+++ mvc-api-angular/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(mvc_api_angular.Startup))]
+
+namespace mvc_api_angular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
